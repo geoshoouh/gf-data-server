fe0e1a25c04808d2e838783d738a2b2753287444
geoshoouh <joshua.d.tate12@gmail.com>
2025-01-26 17:57:22 -0600

    add generated maven files

---
+++ src/test/java/com/gf/server/ServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gf.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
