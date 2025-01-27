888051e413e47053175b74f5294e2251d1cb96ea
geoshoouh <joshua.d.tate12@gmail.com>
2025-01-26 19:36:49 -0600

    add repos

---
+++ src/main/java/com/gf/server/entities/ExerciseInstance.java
@@ -0,0 +1,31 @@
+package com.gf.server.entities;
+
+import com.gf.server.enumerations.ExerciseEnum;
+import com.gf.server.enumerations.ExerciseTypeEnum;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Table(name="EXERCISE_INSTANCES")
+@Data
+public class Exercise_Instance {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id;
+    
+    @ManyToOne
+    private GF_Client client;
+
+    ExerciseTypeEnum exerciseType;
+    ExerciseEnum exercise;
+    
+    int seatSetting;
+    int weight;
+}
---
+++ src/main/java/com/gf/server/repositories/ClientRepository.java
@@ -0,0 +1,7 @@
+package com.gf.server.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ClientRepository extends JpaRepository<GF_Client, Long>{
+    
+}
