32bdfced61dbe1acc1ed28b478a709d5f09ce0a3
geoshoouh <joshua.d.tate12@gmail.com>
2025-01-29 00:58:06 -0600

    add aftereach for service

--- src/test/java/com/gf/server/service_tests/GF_DataManagementServiceTests.java
+++ src/test/java/com/gf/server/service_tests/GF_DataManagementServiceTests.java
@@ -1,5 +1,6 @@
 package com.gf.server.service_tests;
 
+import org.junit.jupiter.api.AfterEach;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -10,4 +11,11 @@ public class GF_DataManagementServiceTests {
     
     @Autowired
     GF_DataManagementService dataManagementService;
+
+    @AfterEach
+    void clearRepositories() {
+        
+        this.dataManagementService.clearClientRepository();
+        this.dataManagementService.clearExerciseRecordRepository();
+    }
 }
