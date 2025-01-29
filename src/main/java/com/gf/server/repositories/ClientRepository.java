6259db471ae0a566f5ed658438825174a9013afb
geoshoouh <joshua.d.tate12@gmail.com>
2025-01-29 00:42:04 -0600

    add get client by email service method

--- src/main/java/com/gf/server/repositories/ClientRepository.java
+++ src/main/java/com/gf/server/repositories/ClientRepository.java
@@ -1,9 +1,12 @@
 package com.gf.server.repositories;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.gf.server.entities.GF_Client;
 
 public interface ClientRepository extends JpaRepository<GF_Client, Long>{
     
+    Optional<GF_Client> findByEmail(String email);
 }
--- src/main/java/com/gf/server/services/GF_DataManagementService.java
+++ src/main/java/com/gf/server/services/GF_DataManagementService.java
@@ -3,8 +3,12 @@ package com.gf.server.services;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.gf.server.dto.ReqResDTO;
+import com.gf.server.entities.GF_Client;
 import com.gf.server.repositories.ClientRepository;
 import com.gf.server.repositories.ExerciseInstanceRepository;
+
+import jakarta.persistence.EntityNotFoundException;
 
 @Service
 public class GF_DataManagementService {
@@ -15,5 +19,20 @@ public class GF_DataManagementService {
     @Autowired
     private ExerciseInstanceRepository exerciseInstanceRepository;
 
-    
+    public ReqResDTO getClientByEmail(ReqResDTO request) throws EntityNotFoundException {
+
+        GF_Client client = this.clientRepository.findByEmail(request.clientEmail()).orElseThrow(() -> new EntityNotFoundException());
+
+        ReqResDTO response = new ReqResDTO(
+            null, 
+            null, 
+            null, 
+            null, 
+            null, 
+            client, 
+            null
+        );
+
+        return response;
+    }
 }
